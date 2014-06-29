99ec43e95c1a2bac09a1fffffc774b44f7ae4ad5
Chibuzor Alumba <coolchibs@gmail.com>
2014-06-28 23:20:25 -0500

    Finally figured out how to display the message received from a socket event into the view/template.

--- client-web/js/clientApp.js
+++ client-web/js/clientApp.js
@@ -2,11 +2,12 @@ App = Ember.Application.create({LOG_TRANSITIONS: true});
 
 App.IndexController = Ember.Controller.extend({
     init: function() {
-        this.initializeSocket();
+        var context = this;
+        this.initializeSocket(context);
     },
     socket: '',
     adminMessage: '',
-    initializeSocket: function(){
+    initializeSocket: function(context){
         console.log('setup controller called');
         var socket = io('http://localhost:8080');
 
@@ -16,12 +17,22 @@ App.IndexController = Ember.Controller.extend({
         });
 
         socket.on('allClients', function (data) {
-            console.log(data['news']);
+            //console.log(data['news']);
+            //adminMessage = data['news'];
             //console.log(data['news'] + " " + adminMessage);
             //controller.set('adminMessage', data);
+            //var controller = this.get('controller');
+            //cont.set('adminMessage', data['news']);
+            //cont('Index').get('adminMessage');
+            //this.set('adminMessage', data['news']);
+            context.set('adminMessage', data['news']);
+            alert(data['news']);
+            //this.adminMsgBox.set(data['news']);
         });
 
+        context.set('adminMessage', "Messages from the admin get placed here");
+
         //assign the socket created to this
-        this.set('socket', socket);
+        context.set('socket', socket);
     }
 });
